d2d9e745aebc3ea2bca53fd4a02bac34742f0c39
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-13 10:44:33 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    //abstract   definicion que no podemos instanciar
+    abstract class Animal
+    {
+        public string Indentificador { get; set; }
+        public string Nombre { get; set; }
+
+        // la utilizamos para modificar el metodo, los metodos abstractos no tienen implementacion.
+        abstract public string desplazar();
+
+    }
+}
---
+++ Herencia/Reptil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Reptil : Animal
+    {
+        public string cambiarPiel()
+        {
+            return Nombre + " cambio de piel";
+        }
+        //override sobrecarga 
+        override public string desplazar()
+        {
+            return Nombre + " esta Caminando";
+        }
+    }
+}
